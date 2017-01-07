f8b08e7cb0ae5e983841f32d977a63776594e7a7
nguyenhoang94it <nguyenhoang94it@gmail.com>
2017-01-07 18:30:16 +0700

    Scanner
    Vr_1

---
+++ Scanner/src/main/java/com/example/hcd_fresher028/scanner/Fragment/FragmentHistory.java
@@ -0,0 +1,11 @@
+package com.example.hcd_fresher028.scanner.Fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by HCD-Fresher028 on 1/6/2017.
+ */
+
+public class FragmentHistory extends Fragment {
+
+}
---
+++ Scanner/src/main/java/com/example/hcd_fresher028/scanner/Fragment/FragmentProduct.java
@@ -0,0 +1,8 @@
+package com.example.hcd_fresher028.scanner.Fragment;
+
+/**
+ * Created by HCD-Fresher028 on 1/6/2017.
+ */
+
+public class FragmentProduct {
+}
